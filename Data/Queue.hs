19cc3f2daf157c43e1be6efac77ffe3ec56a039e
Jim McStanton <jim@jhmcstanton.com>
2021-05-10 14:29:38 -0500

    Helper funcs

--- Data/Queue.hs
+++ Data/Queue.hs
@@ -2,8 +2,10 @@ module Data.Queue
   (
     Queue
   , empty
+  , next
+  , (|>)
   ) where
-import           Protolude            hiding (empty)
+import           Protolude            hiding (empty, on)
 import           Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
 
@@ -11,3 +13,12 @@ newtype Queue a = Q (Seq a) deriving (Eq, Ord, Read, Show)
 
 empty :: Queue a
 empty = Q Seq.empty
+
+(|>) :: Queue a -> a -> Queue a
+q |> x = on (Seq.|> x) q
+
+next :: Queue a -> Queue a
+next = on (Seq.drop 1)
+
+on :: (Seq a -> Seq a) -> Queue a -> Queue a
+on f (Q s) = Q $ f s
--- Data/Types.hs
+++ Data/Types.hs
@@ -1,25 +1,57 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Data.Types
   (
     RoomID(..)
   , UserID(..)
-  , RoomState(..)
+  , RoomState'(..)
+  , RoomState
+  , SpeakReq
+  , clear
+  , clearGlobal
+  , clearLocal
   , newRoom
+  , qGlobal
+  , qLocal
   ) where
-import           Protolude             hiding (Text)
+import           Protolude             hiding (Text, empty, local)
 import           Data.Text.Lazy (Text)
 import           Data.UUID      (UUID)
 
 import           Data.Queue as Q
 
-newtype RoomID   = RoomID { unRoomID   :: UUID } deriving (Eq, Ord, Read, Show)
-newtype UserID   = UserID { unUserID   :: Text } deriving (Eq, Ord, Read, Show)
+newtype RoomID   = RoomID { unRoomID :: UUID } deriving (Eq, Ord, Read, Show)
+newtype UserID   = UserID { unUserID :: Text } deriving (Eq, Ord, Read, Show)
 
+type SpeakReq = UserID
 
-data RoomState = RoomState {
+data RoomState' a = RoomState {
     roomID :: RoomID,
-    global :: Queue UserID,
-    local  :: Queue UserID
+    global :: Queue a,
+    local  :: Queue a
   } deriving (Eq, Ord, Read, Show)
 
-newRoom :: RoomID -> RoomState
+type RoomState = RoomState' SpeakReq
+
+newRoom :: RoomID -> RoomState' a
 newRoom rid = RoomState rid Q.empty Q.empty
+
+qLocal :: a -> RoomState' a -> RoomState' a
+qLocal req = ql (|> req)
+
+qGlobal :: a -> RoomState' a -> RoomState' a
+qGlobal req = qg (|> req)
+
+clearLocal :: RoomState' a -> RoomState' a
+clearLocal = ql (const empty)
+
+clearGlobal :: RoomState' a -> RoomState' a
+clearGlobal = qg (const empty)
+
+clear :: RoomState' a -> RoomState' a
+clear = clearGlobal . clearLocal
+
+ql :: (Queue a -> Queue a) -> RoomState' a -> RoomState' a
+ql queueFunc roomState@RoomState{local} = roomState{local=queueFunc local}
+
+qg :: (Queue a -> Queue a) -> RoomState' a -> RoomState' a
+qg queueFunc roomState@RoomState{global} = roomState{global=queueFunc global}
